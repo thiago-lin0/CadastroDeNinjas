c69ab7d25727915ef1fb4b7b206020d99e96a410
Thiago Lino <thiagolinu@gmail.com>
2025-03-18 18:32:50 -0300

    feat: Coloca o lombok para abstração da classe

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,10 +1,16 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity ele transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -18,36 +24,5 @@ public class NinjaModel {
     @JoinColumn(name = "missao_id")
     private String missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
