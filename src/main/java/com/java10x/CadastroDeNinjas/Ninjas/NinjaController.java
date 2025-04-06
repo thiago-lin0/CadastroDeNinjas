ff609e66d00de7324880004c24a083437b7a9635
Thiago Lino <thiagolinu@gmail.com>
2025-04-06 11:59:25 -0300

    feat: Cria as rotas do controler do ninja mockada

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,34 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa ordem";
+    }
+
+    //Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    // Mostrar ninja por id (READ)
+    @GetMapping("/ninjaID")
+    public String mostrarNinjasPorId(){
+        return "Mostrar Ninjas por ID ";
+    }
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas(){
+        return "Mostrar Todos os Ninjas";
+    }
+
+    //alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninjas";
+    }
+    
+    //deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja Deletado por Id";
     }
 }
