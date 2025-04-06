441f400e12eabbc112a0ee05000491bdc70ebade
Thiago Lino <thiagolinu@gmail.com>
2025-04-06 12:11:09 -0300

    feat: cria as rotas do controler de missoes mockada

--- src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesControler.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesControler.java
@@ -1,4 +1,28 @@
 package com.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesControler {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listada com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissoes(){
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissoes(){
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissoes(){
+        return "Missão deletada com sucesso";
+    }
 }
