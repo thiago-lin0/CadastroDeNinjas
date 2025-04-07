f97df5020506762a99decc5ada2366323244b798
Thiago Lino <thiagolinu@gmail.com>
2025-04-07 16:28:29 -0300

    fix: inclui o @JsonIgnore no arquivo MissoesModel para prevenir o bug de serializacao

--- src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,7 @@
 package com.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -22,6 +24,7 @@ public class MissoesModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 
