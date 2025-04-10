11aa59605f12aef93c92926f96c5bab2edcd41f7
Thiago Lino <thiagolinu@gmail.com>
2025-04-10 15:38:23 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
+import com.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+    private String img_url;
 }
